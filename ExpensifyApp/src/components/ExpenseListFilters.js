f945e4a9552d2df33fb186b3f16220f92d58bcc1
camha91 <crystalhanguyen@gmail.com>
2020-09-10 16:07:51 -0700

    Production Webpack

--- ExpensifyApp/src/components/ExpenseListFilters.js
+++ ExpensifyApp/src/components/ExpenseListFilters.js
@@ -59,11 +59,11 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    setStartDate: (startDate) => dispatch(setStartDate(startDate)),
-    setEndDate: (endDate) => dispatch(setEndDate(endDate)),
     setTextFilter: (text) => dispatch(setTextFilter(text)),
     sortByDate: () => dispatch(sortByDate()),
-    sortByAmount: () => dispatch(sortByAmount())
+    sortByAmount: () => dispatch(sortByAmount()),
+    setStartDate: (startDate) => dispatch(setStartDate(startDate)),
+    setEndDate: (endDate) => dispatch(setEndDate(endDate))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ExpenseListFilters);
