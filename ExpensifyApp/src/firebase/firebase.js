3a1a0918fa2d4b626e8e7dc32827747a63fe1417
camha91 <crystalhanguyen@gmail.com>
2020-09-21 17:39:37 -0700

    Creating a Separate Test Database

--- ExpensifyApp/src/firebase/firebase.js
+++ ExpensifyApp/src/firebase/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAW8UsuyZimeD9nJD3pNYQUfE-Y1M0s4sI",
-    authDomain: "expensify-c49be.firebaseapp.com",
-    databaseURL: "https://expensify-c49be.firebaseio.com",
-    projectId: "expensify-c49be",
-    storageBucket: "expensify-c49be.appspot.com",
-    messagingSenderId: "155869674948",
-    appId: "1:155869674948:web:672aee258303e3d1a57891",
-    measurementId: "G-9DPBNDPVMY"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
